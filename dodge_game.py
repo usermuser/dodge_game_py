d52e76ab02e6c521742813cbcc2af390adfe7032
usermuser <lwrdrstv@gmail.com>
2017-10-16 00:28:23 +0500

    nothing changed

--- dodge_game.py
+++ dodge_game.py
@@ -1,13 +1,10 @@
+#:help vimrc-intro
 import pygame
-
 pygame.init()
-
 display_width = 640
 display_height = 480
-
 gameDisplay = pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption('A bit Racey')
-
 black = (0,0,0)
 white = (255,255,255)
 
@@ -20,12 +17,25 @@ def car(x,y):
 
 x = (display_width * 0.45)
 y = (display_height * 0.8)
+car_speed = 0
+x_change = 0
 
 while not crashed:
 
     for event in pygame.event.get():
-        if event.type == pygame.quit:
+        if event.type == pygame.QUIT:
             crashed = True
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                x_change = -5
+            elif event.key == pygame.K_RIGHT:
+                x_change = +5
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                x_change = 0
+
+    x += x_change
 
     gameDisplay.fill(white)
     car(x,y)
