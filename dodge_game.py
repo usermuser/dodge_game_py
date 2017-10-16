ad8e0c2f3acb584c0be053ec5fcd0ffebc0a2325
usermuser <lwrdrstv@gmail.com>
2017-10-16 10:20:11 +0500

    lesson 5 finished (Displaying text to PyGame screen)

--- dodge_game.py
+++ dodge_game.py
@@ -1,5 +1,6 @@
 #:help vimrc-intro
 import pygame
+import time
 
 pygame.init()
 
@@ -21,13 +22,32 @@ carImg = pygame.image.load('racecar.png')
 def car(x,y):
     gameDisplay.blit(carImg, (x,y))
 
+def text_objects(text, font):
+    textSurface = font.render(text, True, black)
+    return textSurface, textSurface.get_rect()
+
+def message_display(text):
+    largeText = pygame.font.Font('arial.ttf',115)
+    TextSurf, TextRect = text_objects(text, largeText)
+    TextRect.center = ((display_width/2),(display_height/2))
+    gameDisplay.blit(TextSurf, TextRect)
+
+    pygame.display.update()
+
+    time.sleep(2)
+
+    game_loop()
+
+def crash():
+    message_display('You Crashed')
+
 def game_loop():
     x = (display_width * 0.45)
     y = (display_height * 0.8)
 
     x_change = 0
 
-    gameExit = 0
+    gameExit = False
 
     while not gameExit:
 
@@ -50,7 +70,7 @@ def game_loop():
         car(x,y)
 
         if x > display_width - car_width or x <0:
-            gameExit = True
+            crash()
 
         pygame.display.update()
         clock.tick(60)
